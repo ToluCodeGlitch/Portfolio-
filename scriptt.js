aa7802ee768e69ebd3e74ff37a267fd3031a0277
ToluCodeGlitch <africanhorrorstory2@gmail.com>
2025-07-26 15:39:44 +0100

    Update and rename scriptt.js to style.csss

--- scriptt.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("toggleInfoBtn").addEventListener("click", function() {
-  const details = document.getElementById("moreDetails");
-  details.classList.toggle("hidden");
-  this.textContent = details.classList.contains("hidden") ? "More About Me" : "Hide";
-});
